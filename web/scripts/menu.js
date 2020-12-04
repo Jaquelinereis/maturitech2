e6a571c04e77249f5aa5c00e0d2777f85da238c4
jaquelinejmorais@hotmail.com <>
2020-12-04 11:57:48 -0300

    Commit 3

---
+++ web/scripts/menu.js
@@ -0,0 +1,12 @@
+
+       
+      
+       document.write("<a href='cadastrocarro.jsp'><button class='red'>Cadastro carros </button></a> &nbsp;&nbsp; ");
+       document.write("<a href='consultacarro.jsp'><button>Consulta carros</button></a> &nbsp;&nbsp;");
+        document.write("<a href='cadastrocliente.jsp'><button>Cadastro cliente</button></a> &nbsp;&nbsp; ");
+        document.write("<a href='consultacliente.jsp'><button>Consulta cliente</button></a> &nbsp;&nbsp;");
+       document.write ("<a href='cadastrolocacao.jsp'><button>Cadastro locação</button></a> &nbsp;&nbsp;");
+        document.write("<a href='consultalocacao.jsp'><button>Consulta locação</button></a> &nbsp;&nbsp;");
+       document.write("<a href='consultalocacaoResultSet.jsp'><button>Consulta locação RS</button></a> &nbsp;&nbsp;");
+       document.write("<a href='cadastrodevolucao.jsp'><button>Registrar devolução</button></a> &nbsp;&nbsp;");
+       document.write("<hr />");
